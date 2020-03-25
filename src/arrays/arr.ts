0d830849a0e980edd168dcb9f2f129f2fefec570
Gabe Hargrave <gkhargrave@gmail.com>
2020-03-25 15:34:27 -0700

    Minor updates

--- src/arrays/arr.ts
+++ src/arrays/arr.ts
@@ -1,5 +1,5 @@
 /**
- * Generates a new empty Array of `len` length, each filled undefined.
+ * Generates a new empty Array of `len` length, with each index filled with undefined.
  *
  * Use this when you need an Array to map over, but do not need any existing values.
  */
